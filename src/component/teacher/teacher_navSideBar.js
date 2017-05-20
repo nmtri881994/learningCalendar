8ec4723c50119745f427cabab0ab444a341467d3
Tri <nmtri881994@gmail.com>
2017-05-20 19:56:17 +0700

    remove some unnecessary menu from teacher's main navigation

--- src/component/teacher/teacher_navSideBar.js
+++ src/component/teacher/teacher_navSideBar.js
@@ -26,37 +26,37 @@ class NavSideBar extends Component {
                                         </li>
                                     </ul>
                                 </div>
-                                <div className="slide-bar-nav-inner-calendars">
-                                    <div className="slide-bar-nav-inner-heading not-first-heading">
-                                        <strong>Thiết lập</strong>
-                                    </div>
-                                    <ul className="calendars">
-                                        <li>
-                                            <a href="#" className="slide-bar-nav-inner-item-link">
-                                                <div className="slide-bar-nav-inner-item-icon"><i className="fa fa-calendar" aria-hidden="true"></i></div>
-                                                <div className="slide-bar-nav-inner-item-title">1</div>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="#" className="slide-bar-nav-inner-item-link">
-                                                <div className="slide-bar-nav-inner-item-icon"><i className="fa fa-calendar" aria-hidden="true"></i></div>
-                                                <div className="slide-bar-nav-inner-item-title">2</div>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="#" className="slide-bar-nav-inner-item-link">
-                                                <div className="slide-bar-nav-inner-item-icon"><i className="fa fa-calendar" aria-hidden="true"></i></div>
-                                                <div className="slide-bar-nav-inner-item-title">3</div>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="#" className="slide-bar-nav-inner-item-link">
-                                                <div className="slide-bar-nav-inner-item-icon"><i className="fa fa-calendar" aria-hidden="true"></i></div>
-                                                <div className="slide-bar-nav-inner-item-title">4</div>
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </div>
+                                {/*<div className="slide-bar-nav-inner-calendars">*/}
+                                    {/*<div className="slide-bar-nav-inner-heading not-first-heading">*/}
+                                        {/*<strong>Thiết lập</strong>*/}
+                                    {/*</div>*/}
+                                    {/*<ul className="calendars">*/}
+                                        {/*<li>*/}
+                                            {/*<a href="#" className="slide-bar-nav-inner-item-link">*/}
+                                                {/*<div className="slide-bar-nav-inner-item-icon"><i className="fa fa-calendar" aria-hidden="true"></i></div>*/}
+                                                {/*<div className="slide-bar-nav-inner-item-title">1</div>*/}
+                                            {/*</a>*/}
+                                        {/*</li>*/}
+                                        {/*<li>*/}
+                                            {/*<a href="#" className="slide-bar-nav-inner-item-link">*/}
+                                                {/*<div className="slide-bar-nav-inner-item-icon"><i className="fa fa-calendar" aria-hidden="true"></i></div>*/}
+                                                {/*<div className="slide-bar-nav-inner-item-title">2</div>*/}
+                                            {/*</a>*/}
+                                        {/*</li>*/}
+                                        {/*<li>*/}
+                                            {/*<a href="#" className="slide-bar-nav-inner-item-link">*/}
+                                                {/*<div className="slide-bar-nav-inner-item-icon"><i className="fa fa-calendar" aria-hidden="true"></i></div>*/}
+                                                {/*<div className="slide-bar-nav-inner-item-title">3</div>*/}
+                                            {/*</a>*/}
+                                        {/*</li>*/}
+                                        {/*<li>*/}
+                                            {/*<a href="#" className="slide-bar-nav-inner-item-link">*/}
+                                                {/*<div className="slide-bar-nav-inner-item-icon"><i className="fa fa-calendar" aria-hidden="true"></i></div>*/}
+                                                {/*<div className="slide-bar-nav-inner-item-title">4</div>*/}
+                                            {/*</a>*/}
+                                        {/*</li>*/}
+                                    {/*</ul>*/}
+                                {/*</div>*/}
                             </div>
                         </nav>
                     </div>
